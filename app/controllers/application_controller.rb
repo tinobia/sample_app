bf2fcc88536185437be89a38f47ac668726da07e
Nguyen Van Trung <nguyenvantrungnct@gmail.com>
2017-11-09 09:31:18 +0700

    Add Hellooooo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+    render html: "hello, Trung Map hahahaha hihihihi hehehehe hoohohoho!"
+  end
 end
