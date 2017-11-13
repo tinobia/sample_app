e4df816cb58f3002fd757fde23e83565a9cc8792
Nguyen Van Trung <nguyenvantrungnct@gmail.com>
2017-11-13 11:22:11 +0700

    Finish chap 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
