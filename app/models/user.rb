292b60b14636abf1a1f69b1812eb652ef324cf06
Nguyen Van Trung <nguyenvantrungnct@gmail.com>
2017-11-16 15:30:33 +0700

    Finish Chap 8 Basic Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.password.minimum}
+  def self.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
