ce9c8b00121872cf719f224a24ec04715f942497
Nguyen Van Trung <nguyenvantrungnct@gmail.com>
2017-11-20 15:06:55 +0700

    Finish chap 9 advanced login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,8 +12,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }}
     end
     assert_template "users/new"
-    assert_select "div#<CSS id for error explanation>"
-    assert_select "div.<CSS class for field with error>"
   end
 
   test "valid signup information" do
