1e38a272d9136c1b025bb00ff924d43ea200b617
Nguyen Van Trung <nguyenvantrungnct@gmail.com>
2017-11-20 15:06:55 +0700

    Finish chap 10 update users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    login_remember user
+  end
+
+  def login_remember user
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == Settings.session.remem.to_s ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = t("controllers.sessions_controller.danger")
       render :new
@@ -13,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
